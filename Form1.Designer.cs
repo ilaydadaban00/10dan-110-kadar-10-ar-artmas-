df2b609f227c80edf9db256ca713799041ae970a
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-20 00:11:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace aralıklı_artırma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYazdir = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnYazdir
+            // 
+            this.btnYazdir.Location = new System.Drawing.Point(64, 13);
+            this.btnYazdir.Name = "btnYazdir";
+            this.btnYazdir.Size = new System.Drawing.Size(75, 23);
+            this.btnYazdir.TabIndex = 0;
+            this.btnYazdir.Text = "yazdır";
+            this.btnYazdir.UseVisualStyleBackColor = true;
+            this.btnYazdir.Click += new System.EventHandler(this.btnYazdir_Click);
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.Location = new System.Drawing.Point(157, 77);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(120, 95);
+            this.listbox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 254);
+            this.Controls.Add(this.listbox);
+            this.Controls.Add(this.btnYazdir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYazdir;
+        private System.Windows.Forms.ListBox listbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aralıklı_artırma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void btnYazdir_Click(object sender, EventArgs e)
+        {
+            for(int i= 110; i > 10; i=i-10)
+            {
+                listbox.Items.Add(i);   
+            }
+        }
+    }
+}
